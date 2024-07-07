b37f2a5a2f7b0090b4ae9e05c56a770159f473f3
nam nguyen <nguyenvuhainamhan@gmail.com>
2024-07-06 21:34:29 -0700

    switch to local database

--- src/main/java/cmpt276/pg6/realtorest/RealtorestApplication.java
+++ src/main/java/cmpt276/pg6/realtorest/RealtorestApplication.java
@@ -2,18 +2,10 @@ package cmpt276.pg6.realtorest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class RealtorestApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().directory("./etc/secrets").load();
-		System.setProperty("DB_REALTOREST_URL", dotenv.get("DB_REALTOREST_URL"));
-		System.setProperty("DB_REALTOREST_USER", dotenv.get("DB_REALTOREST_USER"));
-		System.setProperty("DB_REALTOREST_PASS", dotenv.get("DB_REALTOREST_PASS"));
-		System.setProperty("GOOGLE_MAPS_API_KEY", dotenv.get("GOOGLE_MAPS_API_KEY"));
-		System.setProperty("MAILGUN_DOMAIN", dotenv.get("MAILGUN_DOMAIN"));
-		System.setProperty("MAILGUN_API_KEY", dotenv.get("MAILGUN_API_KEY"));
 		SpringApplication.run(RealtorestApplication.class, args);
 	}
 }
